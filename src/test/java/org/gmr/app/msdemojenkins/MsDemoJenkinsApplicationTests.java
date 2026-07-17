c24df537fa072fe3aac1aa7e47681af1eac38a6f
Gerardo  mandujano <gmandujano60@outlook.com>
2026-07-17 12:42:59 -0600

    ci: prueba de ap1

--- src/test/java/org/gmr/app/msdemojenkins/MsDemoJenkinsApplicationTests.java
+++ src/test/java/org/gmr/app/msdemojenkins/MsDemoJenkinsApplicationTests.java
@@ -3,11 +3,19 @@ package org.gmr.app.msdemojenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class MsDemoJenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void mainShouldStartApplication() {
+        assertDoesNotThrow(() ->
+                MsDemoJenkinsApplication.main(new String[]{}));
     }
 
 }
